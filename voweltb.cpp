907bdfa2bec8ab479cc9f2ff210fbda08d1854e4
Sayan_surya_99 <55291551+Sayansurya@users.noreply.github.com>
2020-10-01 00:47:42 +0530

    Update voweltb.cpp

--- voweltb.cpp
+++ voweltb.cpp
@@ -1,13 +1,23 @@
 #include<bits/stdc++.h>
+#include <iostream> 
+#include <string> 
 using namespace std;
 int main(){
-	char c;
-	cin>>c;
-	if(c=='A' || c=='E' || c=='I' || c=='O' || c=='U'){
-		cout<<"Vowel"<<endl;
+	string str;
+	int count0=0,count1=0,i,n;
+        cout << "Please enter your name in upper case: \n"; 
+        getline(cin, str);
+        n=str.length();
+	for(i=0;i<n;i++){
+	if(str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U'){
+		count0++;
+		
 	}
 	else{
-		cout<<"Consonant"<<endl;
+		count1++;
+	}	
 	}
+	cout<<"There are"<<count0<<"vowels"<<endl;
+	cout<<"There are"<<count1<<"Consonants"<<endl;
 	return 0;
 }
